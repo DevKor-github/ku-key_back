d7bea09fcd4ee5e702d5996b712d2a80beb9f9bc
DevHeun <dudwo5600@korea.ac.kr>
2024-04-10 16:05:30 +0900

    feat:: course create 기능 추가

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CourseRepository } from './course.repository';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { CourseEntity } from 'src/entities/course.entity';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+    constructor(
+        @InjectRepository(CourseRepository)
+        private courseRepository: CourseRepository,
+    ){}
+
+    async createCourse(createCourseDto: CreateCourseDto): Promise<CourseEntity>{
+        return await this.courseRepository.createCourse(createCourseDto);
+    }
+}
