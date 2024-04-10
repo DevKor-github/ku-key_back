138398b88ba3ab08e1e0ce21093d0e34be4bcbc3
DevHeun <dudwo5600@korea.ac.kr>
2024-04-11 01:32:04 +0900

    feat:: 검색기능 추가 - 전공 학과(학부) 입력 시 해당하는 과목만 조회 - 추후에 여러 검색 옵션 추가 필요

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -9,37 +9,51 @@ import { CourseDetailRepository } from './course-detail.repository';
 
 @Injectable()
 export class CourseService {
-    constructor(
-        @InjectRepository(CourseRepository)
-        private courseRepository: CourseRepository,
+  constructor(
+    @InjectRepository(CourseRepository)
+    private courseRepository: CourseRepository,
 
-        @InjectRepository(CourseDetailRepository)
-        private courseDetailRepository : CourseDetailRepository,
-    ){}
+    @InjectRepository(CourseDetailRepository)
+    private courseDetailRepository: CourseDetailRepository,
+  ) {}
 
-    async createCourse(createCourseDto: CreateCourseDto): Promise<CourseEntity>{
-        return await this.courseRepository.createCourse(createCourseDto);
+  async createCourse(createCourseDto: CreateCourseDto): Promise<CourseEntity> {
+    return await this.courseRepository.createCourse(createCourseDto);
+  }
+
+  async createCourseDetail(
+    createCourseDetailDto: CreateCourseDetailDto,
+  ): Promise<CourseDetailEntity> {
+    try {
+      // Check if course exists
+      await this.courseRepository.findOne({
+        where: { id: createCourseDetailDto.courseId },
+      });
+      return await this.courseDetailRepository.createCourseDetail(
+        createCourseDetailDto,
+      );
+    } catch (error) {
+      throw new NotFoundException(
+        `Course with id ${createCourseDetailDto.courseId} not found`,
+      );
     }
+  }
 
-    async createCourseDetail(createCourseDetailDto: CreateCourseDetailDto): Promise<CourseDetailEntity>{
-        try{
-            // Check if course exists
-            await this.courseRepository.findOne({
-                where : {id : createCourseDetailDto.courseId}
-            });
-            return await this.courseDetailRepository.createCourseDetail(createCourseDetailDto);
-        }catch(error){
-            throw new NotFoundException(`Course with id ${createCourseDetailDto.courseId} not found`);
-        }
-    }
+  async getAllCourses(): Promise<CourseEntity[]> {
+    return await this.courseRepository.find();
+  }
 
-    async getAllCourses() : Promise<CourseEntity[]>{
-        return await this.courseRepository.find();
-    }
+  async getCourse(courseId: number): Promise<CourseEntity> {
+    return await this.courseRepository.findOne({
+      where: { id: courseId },
+    });
+  }
 
-    async getCourse(courseId : number) : Promise<CourseEntity>{
-        return await this.courseRepository.findOne({
-            where:{id:courseId},
-        });
-    }
+  async getCourseSearch(search: string): Promise<CourseEntity[]> {
+    return await this.courseRepository.find({
+      where: {
+        major: search,
+      },
+    });
+  }
 }
