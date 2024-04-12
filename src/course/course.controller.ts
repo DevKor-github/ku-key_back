81f1872b38eeba7726ab294a3cf9804ee31ec9fe
DevHeun <dudwo5600@korea.ac.kr>
2024-04-12 15:10:02 +0900

    feat:: course 업데이트 기능 추가 - PartialType으로 CreateCourseDto를 UpdateCourseDto로 종속받음

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,40 +1,51 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { CourseEntity } from 'src/entities/course.entity';
 import { CourseService } from './course.service';
 import { CourseDetailEntity } from 'src/entities/course-detail.entity';
 import { CreateCourseDetailDto } from './dto/create-course-detail.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
 
 @Controller('course')
 export class CourseController {
-    constructor(private courseService: CourseService){}
+  constructor(private courseService: CourseService) {}
 
-    @Post()
-    async createCourse(
-        @Body() createCourseDto : CreateCourseDto,
-    ): Promise<CourseEntity>{
-        return await this.courseService.createCourse(createCourseDto);
-    }
+  @Post()
+  async createCourse(
+    @Body() createCourseDto: CreateCourseDto,
+  ): Promise<CourseEntity> {
+    return await this.courseService.createCourse(createCourseDto);
+  }
 
-    @Post('detail')
-    async createCourseDetail(
-        @Body() createCourseDetailDto : CreateCourseDetailDto,
-    ) : Promise<CourseDetailEntity> {
-        return await this.courseService.createCourseDetail(createCourseDetailDto);
-    }
+  @Post('detail')
+  async createCourseDetail(
+    @Body() createCourseDetailDto: CreateCourseDetailDto,
+  ): Promise<CourseDetailEntity> {
+    return await this.courseService.createCourseDetail(createCourseDetailDto);
+  }
 
-    @Get()
-    async getCourseSearch(@Query('search') search: string) :Promise<CourseEntity[]>{
-        return await this.courseService.getCourseSearch(search);
-    }
+  @Get()
+  async getCourseSearch(
+    @Query('search') search: string,
+  ): Promise<CourseEntity[]> {
+    return await this.courseService.getCourseSearch(search);
+  }
 
-    @Get()
-    async getAllCourses() : Promise<CourseEntity[]>{
-        return await this.courseService.getAllCourses();
-    }
+  @Get()
+  async getAllCourses(): Promise<CourseEntity[]> {
+    return await this.courseService.getAllCourses();
+  }
 
-    @Get('/:courseId')
-    async getCourse(@Param('courseId') courseId : number) : Promise<CourseEntity>{
-        return await this.courseService.getCourse(courseId);
-    }
+  @Get('/:courseId')
+  async getCourse(@Param('courseId') courseId: number): Promise<CourseEntity> {
+    return await this.courseService.getCourse(courseId);
+  }
+
+  @Patch('/:courseId')
+  async updateCourse(
+    @Param('courseId') courseId: number,
+    @Body() updateCourseDto: UpdateCourseDto,
+  ): Promise<CourseEntity> {
+    return await this.courseService.updateCourse(updateCourseDto, courseId);
+  }
 }
--- src/course/course.repository.ts
+++ src/course/course.repository.ts
@@ -1,17 +1,25 @@
-import { Injectable } from "@nestjs/common";
-import { CourseEntity } from "src/entities/course.entity";
-import { DataSource, Repository } from "typeorm";
-import { CreateCourseDto } from "./dto/create-course.dto";
+import { Injectable } from '@nestjs/common';
+import { CourseEntity } from 'src/entities/course.entity';
+import { DataSource, Repository } from 'typeorm';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
 
 @Injectable()
-export class CourseRepository extends Repository<CourseEntity>{
-    constructor(dataSource : DataSource){
-        super(CourseEntity, dataSource.createEntityManager());
-    }
+export class CourseRepository extends Repository<CourseEntity> {
+  constructor(dataSource: DataSource) {
+    super(CourseEntity, dataSource.createEntityManager());
+  }
 
-    async createCourse(createCourseDto: CreateCourseDto) : Promise<CourseEntity>{
-        const course = this.create(createCourseDto);
-        return await this.save(course);
-    }
+  async createCourse(createCourseDto: CreateCourseDto): Promise<CourseEntity> {
+    const course = this.create(createCourseDto);
+    return await this.save(course);
+  }
 
+  async updateCourse(updateCourseDto : UpdateCourseDto, courseId: number): Promise<any> {
+    const course = await this.findOne({
+      where: { id: courseId },
+    });
+    const updatedCourse = Object.assign(course, updateCourseDto);
+    return await this.save(updatedCourse);
+  }
 }
