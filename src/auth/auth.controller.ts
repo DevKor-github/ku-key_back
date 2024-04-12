6a58e57e7f30ccce5e6a307dab7cd20843c52744
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-04-13 01:15:00 +0900

    feat::이메일 인증 Dto 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,6 +4,8 @@ import { AuthService } from './auth.service';
 import { RefreshAuthGuard } from './guards/refresh-auth-guard';
 import { User } from 'src/decorators/user.decorator';
 import { AuthorizedUserDto } from './dto/authorized-user-dto';
+import { VerificationRequestDto } from './dto/verification-request.dto';
+import { VerifyEmailRequestDto } from './dto/verify-email-request.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -24,12 +26,12 @@ export class AuthController {
   }
 
   @Post('verification-request')
-  async sendVerification(@Body() body) {
+  async sendVerification(@Body() body: VerificationRequestDto) {
     return await this.authService.sendVerification(body.email);
   }
 
   @Post('verify-email')
-  async VerifyEmailByToken(@Body() body) {
+  async VerifyEmailByToken(@Body() body: VerifyEmailRequestDto) {
     return await this.authService.verifyEmail(body.email, body.verifyToken);
   }
 }
