f11ec8315a393b590e04adf1086d402148c1c1bd
DevHeun <dudwo5600@korea.ac.kr>
2024-04-12 18:07:00 +0900

    feat:: timetable에 course 등록 기능 추가 - timetable id와 course id를 쿼리로 받음 - 따로 에러처리 로직은 아직 구현 X

--- src/timetable/timetable.controller.ts
+++ src/timetable/timetable.controller.ts
@@ -1,11 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Query } from '@nestjs/common';
 import { TimeTableEntity } from 'src/entities/timetable.entity';
 import { CreateTimeTableDto } from './dto/create-timetable.dto';
 import { TimetableService } from './timetable.service';
+import { TimeTableCourseEntity } from 'src/entities/timetable-course.entity';
 
 @Controller('timetable')
 export class TimetableController {
     constructor(private timeTableService:TimetableService) {}
+
+    // timetable 에 course 추가
+    @Post('course')
+    async createTimeTableCourse(
+        @Query('timeTableId') timeTableId : number,
+        @Query('courseId') courseId : number,
+    ) : Promise<TimeTableCourseEntity> {
+        return await this.timeTableService.createTimeTableCourse(timeTableId, courseId);
+    
+    }
 
     @Post()
     async createTimeTable(
@@ -13,4 +24,5 @@ export class TimetableController {
     ) : Promise<TimeTableEntity>{
         return await this.timeTableService.createTimeTable(createTimeTableDto);
     }
+
 }
