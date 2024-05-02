318d6baa00be004ff0ae2931a79661a509c0963c
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-05-03 02:33:23 +0900

    refactor:: 인증기능을 위한 Dto 추가

--- src/auth/dto/verification-request.dto.ts
+++ src/auth/dto/verification-request.dto.ts
@@ -1,8 +1,8 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class VerificationRequestDto {
-  @IsString()
   @IsNotEmpty()
+  @IsString()
   @IsEmail()
   email: string;
 }
