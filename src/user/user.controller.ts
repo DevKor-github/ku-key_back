6212a1860801b68227c45c4a7679f5c86d277059
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-05-03 12:15:11 +0900

    feat::프로필 설정, 조회 기능 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,21 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserRequestDto } from './dto/create-user-request.dto';
 import { checkPossibleResponseDto } from './dto/check-possible-response.dto';
 import { CreateUserResponseDto } from './dto/create-user-response.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ProfileDto } from './dto/profile.dto';
+import { User } from 'src/decorators/user.decorator';
+import { AuthorizedUserDto } from 'src/auth/dto/authorized-user-dto';
+import { SetProfileResponseDto } from './dto/set-profile-response.dto';
 
 @Controller('user')
 export class UserController {
@@ -27,5 +40,22 @@ export class UserController {
     @Param('email') email: string,
   ): Promise<checkPossibleResponseDto> {
     return await this.userService.checkEmailPossible(email);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('/profile')
+  async setProfile(
+    @Body() profileDto: ProfileDto,
+    @User() user: AuthorizedUserDto,
+  ): Promise<SetProfileResponseDto> {
+    const id = user.id;
+    return await this.userService.setProfile(id, profileDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/profile')
+  async getProfile(@User() user: AuthorizedUserDto): Promise<ProfileDto> {
+    const id = user.id;
+    return await this.userService.getProfile(id);
   }
 }
