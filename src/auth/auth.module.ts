58b327d8d9e36f533b7d59f652a9de8a3e5ccbec
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-05-08 14:52:15 +0900

    feat:: KuVerification 레포지토리 생성 및 auth모듈에 의존성 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,6 +8,12 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RefreshStrategy } from './strategies/refresh.strategy';
 import { EmailService } from './email.service';
+import { MulterModule } from '@nestjs/platform-express';
+import { MulterConfigService } from './multer.config';
+import { VerifyStrategy } from './strategies/verify.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { KuVerificationEntity } from 'src/entities/ku-verification.entity';
+import { KuVerificationRepository } from './ku-verification.repository';
 
 @Module({
   imports: [
@@ -21,15 +27,21 @@ import { EmailService } from './email.service';
         };
       },
     }),
+    MulterModule.registerAsync({
+      useClass: MulterConfigService,
+    }),
+    TypeOrmModule.forFeature([KuVerificationEntity]),
   ],
   controllers: [AuthController],
   providers: [
     AuthService,
     EmailService,
     UserRepository,
+    KuVerificationRepository,
     LocalStrategy,
     JwtStrategy,
     RefreshStrategy,
+    VerifyStrategy,
   ],
 })
 export class AuthModule {}
