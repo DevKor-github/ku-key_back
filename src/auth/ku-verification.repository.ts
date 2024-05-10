2c59b635296cf7a10cd0362541f079494efa254a
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-05-10 13:00:44 +0900

    feat:: 학번으로 요청 조회 및 요청 변경 기능 추가

--- src/auth/ku-verification.repository.ts
+++ src/auth/ku-verification.repository.ts
@@ -9,16 +9,47 @@ export class KuVerificationRepository extends Repository<KuVerificationEntity> {
     super(KuVerificationEntity, dataSource.createEntityManager());
   }
 
+  async findRequestsByStudentNumber(
+    studentNumber: number,
+  ): Promise<KuVerificationEntity[]> {
+    const requests = await this.find({
+      where: { studentNumber: studentNumber },
+    });
+    return requests;
+  }
+
+  async findRequestByUser(user: UserEntity): Promise<KuVerificationEntity> {
+    const request = await this.findOne({
+      where: { user: { id: user.id } },
+    });
+    return request;
+  }
+
   async createVerificationRequest(
     imgDir: string,
     studentNumber: number,
     user: UserEntity,
   ) {
-    const request = this.create({
+    const newRequest = this.create({
       imgDir: imgDir,
       studentNumber: studentNumber,
       user: user,
     });
-    return await this.save(request);
+    return await this.save(newRequest);
+  }
+
+  async modifyVerificationRequest(
+    originReqeust: KuVerificationEntity,
+    imgDir: string,
+    studentNumber: number,
+    user: UserEntity,
+  ) {
+    return await this.update(
+      { id: originReqeust.id, user: user },
+      {
+        imgDir: imgDir,
+        studentNumber: studentNumber,
+      },
+    );
   }
 }
