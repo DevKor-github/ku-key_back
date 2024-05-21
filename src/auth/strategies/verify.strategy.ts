000e6308ef7d41fcc87d7c5368ff34b5f0356c91
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-05-21 17:46:59 +0900

    refactor:: auth 모듈에서 userService 를 통해서만 User Entity에 접근할 수 있도록 수정

--- src/auth/strategies/verify.strategy.ts
+++ src/auth/strategies/verify.strategy.ts
@@ -3,13 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthorizedUserDto } from '../dto/authorized-user-dto';
-import { AuthService } from '../auth.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class VerifyStrategy extends PassportStrategy(Strategy, 'verify') {
   constructor(
     private readonly configService: ConfigService,
-    private readonly authService: AuthService,
+    private readonly userService: UserService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -19,7 +19,7 @@ export class VerifyStrategy extends PassportStrategy(Strategy, 'verify') {
   }
 
   async validate(payload: any): Promise<AuthorizedUserDto> {
-    const isVerified = await this.authService.checkUserVerified(payload.id);
+    const isVerified = await this.userService.checkUserVerified(payload.id);
     if (isVerified) {
       throw new BadRequestException('user is already verified!');
     }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -9,5 +9,6 @@ import { UserRepository } from './user.repository';
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService, UserRepository],
+  exports: [UserService],
 })
 export class UserModule {}
