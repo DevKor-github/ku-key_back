5619c03995f0c3590838b60345b8484915c7eed5
Devheun <dudwo5600@korea.ac.kr>
2024-05-28 01:23:10 +0900

    feat:: 개인 스케쥴 등록 API 추가 - 아직 시간표에 등록된 강의, 스케쥴과 시간 겹치는지 체크 X - 단순히 개인 스케쥴 등록만

---
+++ src/schedule/schedule.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { ScheduleService } from './schedule.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ScheduleEntity } from 'src/entities/schedule.entity';
+import { User } from 'src/decorators/user.decorator';
+import { AuthorizedUserDto } from 'src/auth/dto/authorized-user-dto';
+import { CreateScheduleRequestDto } from './dto/create-schedule-request.dto';
+
+@Controller('schedule')
+@UseGuards(JwtAuthGuard) // 시간표 관련 API는 인증 필요해서 JwtAuthGuard 사용
+export class ScheduleController {
+  constructor(private readonly scheduleService: ScheduleService) {}
+
+  // 스케쥴 추가 (강의 시간과 다른 스케쥴 시간과 안겹치게 추가하는 로직 구현 필요함)
+  @Post('/:timeTableId')
+  async createSchedule(
+    @User() user: AuthorizedUserDto,
+    @Param('timeTableId') timeTableId: number,
+    @Body() createScheduleRequestDto: CreateScheduleRequestDto,
+  ): Promise<ScheduleEntity> {
+    return await this.scheduleService.createSchedule(
+      timeTableId,
+      createScheduleRequestDto,
+      user,
+    );
+  }
+}
