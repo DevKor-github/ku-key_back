897df7a60a193ac7a4c06e5dd4f21f812ecf97e6
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-05-29 13:04:53 +0900

    mod:: 이메일 인증 본문 html 태그 추가

--- src/auth/email.service.ts
+++ src/auth/email.service.ts
@@ -31,8 +31,13 @@ export class EmailService {
     const mailOptions: EmailOptions = {
       from: this.configService.get('EMAIL_USER'),
       to: email,
-      subject: '[Ku-Key] 인증코드를 확인해주세요',
-      html: `인증번호 : ${verifyToken}`,
+      subject: '[Ku-Key] Please check your verification code',
+      html: `<div style="text-align: center;">
+              <h1>🍪KuKey Verification Code</h1>
+              <p>This is the code to verify that your email is possible.</p>
+              <p>Please finish sign-up by input this code.</p>
+              <h2>${verifyToken}</h2>
+            </div>`,
     };
 
     return await this.transporter.sendMail(mailOptions);
