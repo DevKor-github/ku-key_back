953669bc16508f3d8eef484e61f56460b81c0f34
Devheun <dudwo5600@korea.ac.kr>
2024-06-20 16:21:48 +0900

    refactor:: timeTableId로 시간표 조회 하는 return 타입 변경 - 기존에는 timeTableId로 시간표 조회 시 강의 관련 정보만 return 되었다면 - 강의 관련 정보, 일정 관련 정보 다 return 하도록 DTO 생성 후 통합

---
+++ src/timetable/dto/get-timetable-timetable.dto.ts
@@ -0,0 +1,11 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { GetCourseInfoByTimeTableIdResponseDto } from './get-courseinfo-timetable.dto';
+import { GetScheduleInfoByTimeTableIdResponseDto } from './get-scheduleinfo-timetable.dto';
+
+export class GetTimeTableByTimeTableIdDto {
+  @ApiPropertyOptional({ type: GetCourseInfoByTimeTableIdResponseDto })
+  courses?: GetCourseInfoByTimeTableIdResponseDto[];
+
+  @ApiPropertyOptional({ type: [GetScheduleInfoByTimeTableIdResponseDto] })
+  schedules?: GetScheduleInfoByTimeTableIdResponseDto[];
+}
