096184143f40b38b69f97dd2b0241875dc1a05fa
Devheun <dudwo5600@korea.ac.kr>
2024-06-20 16:21:48 +0900

    refactor:: timeTableId로 시간표 조회 하는 return 타입 변경 - 기존에는 timeTableId로 시간표 조회 시 강의 관련 정보만 return 되었다면 - 강의 관련 정보, 일정 관련 정보 다 return 하도록 DTO 생성 후 통합

---
+++ src/timetable/dto/get-courseinfo-timetable.dto.ts
@@ -0,0 +1,62 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+const DayType = {
+  Mon: 'Mon',
+  Tue: 'Tue',
+  Wed: 'Wed',
+  Thu: 'Thu',
+  Fri: 'Fri',
+  Sat: 'Sat',
+  Sun: 'Sun',
+} as const;
+
+export type DayType = (typeof DayType)[keyof typeof DayType];
+
+export class GetCourseInfoByTimeTableIdResponseDto {
+  @ApiProperty({ description: '강의 ID' })
+  @IsNumber()
+  @IsNotEmpty()
+  courseId: number;
+
+  @ApiProperty({ description: '교수님 성함' })
+  @IsString()
+  @IsNotEmpty()
+  professorName: string;
+
+  @ApiProperty({ description: '강의명' })
+  @IsString()
+  @IsNotEmpty()
+  courseName: string;
+
+  @ApiProperty({ description: '학수 번호' })
+  @IsString()
+  @IsNotEmpty()
+  courseCode: string;
+
+  @ApiProperty({ description: '시작 시간' })
+  @IsString()
+  @IsOptional()
+  startTime: string;
+
+  @ApiProperty({ description: '종료 시간' })
+  @IsString()
+  @IsOptional()
+  endTime: string;
+
+  @ApiProperty({ description: '강의실' })
+  @IsString()
+  @IsOptional()
+  classroom: string;
+
+  @ApiProperty({ description: '요일' })
+  @IsEnum(DayType)
+  @IsOptional()
+  day: DayType;
+}
