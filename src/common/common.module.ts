1141b32bcf366cad77f31edf3cf10f45d130039e
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-06-20 23:02:19 +0900

    feat:: 번역 기능 추가
    - Deepl API 사용
    - 한국어, 영어로 번역하는 기능 추가

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FileService } from './file.service';
 import { EmailService } from './email.service';
+import { TranslateService } from './translate.service';
 
 @Module({
-  providers: [FileService, EmailService],
-  exports: [FileService, EmailService],
+  providers: [FileService, EmailService, TranslateService],
+  exports: [FileService, EmailService, TranslateService],
 })
 export class CommonModule {}
