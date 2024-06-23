8690aaf09b8ad0e2b62c37feb041e284c410bf9f
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-06-23 22:04:29 +0900

    feat:: Community 기능에 필요한 Entity 생성
    - Post Entity(게시글)
    - Post-Image Entity(게시글 첨부 이미지)
    - Board Entity(게시판 종류)
    - Comment Entity(댓글)
    - 관계 설정을 위해 User Entity 변경

---
+++ src/entities/board.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { CommonEntity } from './common.entity';
+import { PostEntity } from './post.entity';
+
+@Entity('board')
+export class BoardEntity extends CommonEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column()
+  description: string;
+
+  @OneToMany(() => PostEntity, (postEntity) => postEntity.board)
+  posts: PostEntity[];
+}
