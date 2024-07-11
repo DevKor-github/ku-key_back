eb9c11203203246be3b64e6dce1cecff85a4ff01
JeongYeonSeung <asas4654@daum.net>
2024-07-11 14:00:52 +0900

    refactor:: 응답 dto에서 불필요한 검증 데코레이터 삭제

--- src/home/club/dto/get-club-response.dto.ts
+++ src/home/club/dto/get-club-response.dto.ts
@@ -1,49 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class GetClubResponseDto {
-  @IsNumber()
-  @IsNotEmpty()
   @ApiProperty({ description: 'club table의 PK' })
   clubId: number;
 
-  @IsString()
-  @IsNotEmpty()
   @ApiProperty({ description: '동아리명' })
   name: string;
 
-  @IsString()
-  @IsNotEmpty()
   @ApiProperty({ description: '동아리 요약' })
   summary: string;
 
-  @IsString()
-  @IsNotEmpty()
   @ApiProperty({ description: '정기 모임' })
   regularMeeting: string;
 
-  @IsString()
-  @IsNotEmpty()
   @ApiProperty({ description: '모집 기간' })
   recruitmentPeriod: string;
 
-  @IsString()
-  @IsNotEmpty()
   @ApiProperty({ description: '동아리 설명' })
   description: string;
 
-  @IsString()
-  @IsNotEmpty()
   @ApiProperty({ description: '동아리 사진 URL' })
   imageUrl: string;
 
-  @IsNumber()
-  @IsNotEmpty()
   @ApiProperty({ description: '좋아요 개수' })
   likeCount: number;
 
-  @IsBoolean()
-  @IsNotEmpty()
   @ApiProperty({ description: '좋아요 여부' })
   isLiked: boolean;
 }
