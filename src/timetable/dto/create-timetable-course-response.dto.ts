b3a72e4481a9b78b252f2912e4057c6e3b3ffd1c
Devheun <dudwo5600@korea.ac.kr>
2024-07-26 11:00:23 +0900

    refactor:: timetable response dto class-validator 삭제

--- src/timetable/dto/create-timetable-course-response.dto.ts
+++ src/timetable/dto/create-timetable-course-response.dto.ts
@@ -1,19 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateTimetableCourseResponseDto {
   @ApiProperty({ description: 'ID' })
-  @IsNumber()
-  @IsNotEmpty()
   id: number;
 
   @ApiProperty({ description: '시간표 ID' })
-  @IsNumber()
-  @IsNotEmpty()
   timetableId: number;
 
   @ApiProperty({ description: '강의 ID' })
-  @IsNumber()
-  @IsNotEmpty()
   courseId: number;
 }
--- src/timetable/dto/get-scheduleinfo-timetable.dto.ts
+++ src/timetable/dto/get-scheduleinfo-timetable.dto.ts
@@ -1,11 +1,4 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEnum,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-} from 'class-validator';
 
 const DayType = {
   Mon: 'Mon',
@@ -21,32 +14,20 @@ export type DayType = (typeof DayType)[keyof typeof DayType];
 
 export class GetScheduleInfoByTimetableIdResponseDto {
   @ApiProperty({ description: '일정 ID' })
-  @IsNumber()
-  @IsNotEmpty()
   scheduleId: number;
 
   @ApiProperty({ description: '일정 이름' })
-  @IsString()
-  @IsNotEmpty()
   scheduleTitle: string;
 
   @ApiProperty({ description: '일정 시작 시간' })
-  @IsString()
-  @IsNotEmpty()
   scheduleStartTime: string;
 
   @ApiProperty({ description: '일정 종료 시간' })
-  @IsString()
-  @IsNotEmpty()
   scheduleEndTime: string;
 
   @ApiProperty({ description: '일정 장소' })
-  @IsString()
-  @IsOptional()
   location: string;
 
   @ApiProperty({ description: '일정 요일' })
-  @IsEnum(DayType)
-  @IsNotEmpty()
   scheduleDay: DayType;
 }
