0ed339910465dd5a3de94c265d9833c4dcfce337
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-08-12 19:56:26 +0900

    fix:: 게시글 수정 시 이미지 변경 여부 오류 해결
    - formData에서 boolean으로 받아오기 위해 ToBoolean 데코레이터 사용

--- src/community/post/dto/update-post.dto.ts
+++ src/community/post/dto/update-post.dto.ts
@@ -1,10 +1,11 @@
-import { IsBoolean, IsNotEmpty } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 import { CreatePostRequestDto } from './create-post.dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { ToBoolean } from 'src/decorators/to-boolean.decorator';
 
 export class UpdatePostRequestDto extends CreatePostRequestDto {
   @IsNotEmpty()
-  @IsBoolean()
+  @ToBoolean()
   @ApiProperty({ description: '첨부 이미지 변경 여부' })
   imageUpdate: boolean;
 }
