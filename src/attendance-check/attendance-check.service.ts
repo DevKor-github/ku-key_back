29f6a1d8990afd79b3f3c3e5c22bb67d52580d86
Devheun <dudwo5600@korea.ac.kr>
2024-08-16 13:12:40 +0900

    refactor:: 출석체크 에러 타입 변경 - 이미 출석을 했을 시에 권한관련 에러인 Forbidden 보다는 Conflict가 더 Restful 한 것으로 판단하여 변경

--- src/attendance-check/attendance-check.service.ts
+++ src/attendance-check/attendance-check.service.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { AttendanceCheckEntity } from 'src/entities/attendance-check.entity';
 import { EntityManager } from 'typeorm';
 import { TakeAttendanceResponseDto } from './dto/take-attendance.dto';
@@ -27,7 +27,7 @@ export class AttendanceCheckService {
       },
     );
     if (isAlreadyAttended) {
-      throw new ForbiddenException('Already attended');
+      throw new ConflictException('Already attended');
     }
 
     const attendance = transactionManager.create(AttendanceCheckEntity, {
