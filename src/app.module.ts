6c7c74a1ffa63856b8fa90d18adf83019dd67019
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2024-10-05 18:20:02 +0900

    feat:: redis password 환경변수 통해 가져오도록 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -56,6 +56,7 @@ console.log(`.env.${process.env.NODE_ENV}`);
         store: redisStore,
         host: configService.get('REDIS_HOST'),
         port: configService.get('REDIS_PORT'),
+        password: configService.get('REDIS_PASSWORD'),
       }),
     }),
     CommonModule,
