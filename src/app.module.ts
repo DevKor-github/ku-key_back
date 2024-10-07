5e7c006be466a3fc90a1fb7b6584997c0f224885
Devheun <dudwo5600@korea.ac.kr>
2024-10-07 16:40:28 +0900

    fix: 안쓰는 import 구문 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
