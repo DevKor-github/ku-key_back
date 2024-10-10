faea100fb6f1a3d4e0114ea5517de6715eb0e9bc
Devheun <dudwo5600@korea.ac.kr>
2024-10-10 21:28:52 +0900

    refactor:: let에서 const로 선언방식 변경

--- src/utils/user-id-extractor.util.ts
+++ src/utils/user-id-extractor.util.ts
@@ -1,5 +1,4 @@
 export function extractUserId(request: any): string {
-  let tokenPayload;
   let userId: string = 'Not Logged In';
 
   // 로그인한 사용자의 경우 - userId 바로 추출
@@ -10,7 +9,7 @@ export function extractUserId(request: any): string {
     const splitedTokens = request.headers['authorization']
       .split(' ')[1]
       .split('.');
-    tokenPayload = JSON.parse(atob(splitedTokens[1]));
+    const tokenPayload = JSON.parse(atob(splitedTokens[1]));
     userId = tokenPayload.id;
   }
   return userId;
