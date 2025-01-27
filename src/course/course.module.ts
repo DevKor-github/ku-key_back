1dbcec1817194c9407e12bd13263847910347c0f
Devheun <86945971+Devheun@users.noreply.github.com>
2025-01-27 21:47:27 +0900

    refactor:: 모듈 등록

--- src/course/course.module.ts
+++ src/course/course.module.ts
@@ -6,11 +6,43 @@ import { CourseDetailRepository } from './course-detail.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseEntity } from 'src/entities/course.entity';
 import { CourseDetailEntity } from 'src/entities/course-detail.entity';
+import { AcademicFoundationSearchStrategy } from './strategy/academic-foundation-search-strategy';
+import { AllCoursesSearchStrategy } from './strategy/all-courses-search-strategy';
+import { GeneralSearchStrategy } from './strategy/general-search-strategy';
+import { MajorSearchStrategy } from './strategy/major-search-strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CourseEntity, CourseDetailEntity])],
   controllers: [CourseController],
-  providers: [CourseService, CourseRepository, CourseDetailRepository],
+  providers: [
+    CourseService,
+    CourseRepository,
+    CourseDetailRepository,
+    AcademicFoundationSearchStrategy,
+    AllCoursesSearchStrategy,
+    GeneralSearchStrategy,
+    MajorSearchStrategy,
+    {
+      provide: 'CourseSearchStrategy',
+      useFactory: (
+        academicFoundationSearchStrategy: AcademicFoundationSearchStrategy,
+        allCoursesSearchStrategy: AllCoursesSearchStrategy,
+        generalSearchStrategy: GeneralSearchStrategy,
+        majorSearchStrategy: MajorSearchStrategy,
+      ) => [
+        academicFoundationSearchStrategy,
+        allCoursesSearchStrategy,
+        generalSearchStrategy,
+        majorSearchStrategy,
+      ],
+      inject: [
+        AcademicFoundationSearchStrategy,
+        AllCoursesSearchStrategy,
+        GeneralSearchStrategy,
+        MajorSearchStrategy,
+      ],
+    },
+  ],
   exports: [CourseService],
 })
 export class CourseModule {}
