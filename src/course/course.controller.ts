76881fcb94a8cdc13c233ac0f47da04aa426382d
Devheun <86945971+Devheun@users.noreply.github.com>
2025-01-27 21:45:58 +0900

    refactor:: Course 컨트롤러 수정 및 데코레이터 수정

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,17 +1,10 @@
-import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { ApiTags } from '@nestjs/swagger';
-import { CommonCourseResponseDto } from './dto/common-course-response.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { SearchCourseCodeDto } from './dto/search-course-code.dto';
-import { SearchCourseNameDto } from './dto/search-course-name.dto';
-import { SearchProfessorNameDto } from './dto/search-professor-name.dto';
 import { PaginatedCoursesDto } from './dto/paginated-courses.dto';
 import { CourseDocs } from 'src/decorators/docs/course.decorator';
-import { GetGeneralCourseDto } from './dto/get-general-course.dto';
-import { GetMajorCourseDto } from './dto/get-major-course.dto';
-import { GetAcademicFoundationCourseDto } from './dto/get-academic-foundation-course.dto';
-import { SearchCoursesWithKeywordDto } from './dto/search-courses-with-keyword.dto';
+import { SearchCourseNewDto } from './dto/search-course-new.dto';
 
 @ApiTags('course')
 @CourseDocs
@@ -20,139 +13,10 @@ export class CourseController {
   constructor(private courseService: CourseService) {}
 
   @UseGuards(JwtAuthGuard)
-  @Get('search-all')
-  async searchAllCourses(
-    @Query() searchCoursesWithKeywordDto: SearchCoursesWithKeywordDto,
+  @Get()
+  async searchCourses(
+    @Query() searchCourseNewDto: SearchCourseNewDto,
   ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.searchAllCourses(
-      searchCoursesWithKeywordDto,
-    );
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('search-major')
-  async searchMajorCourses(
-    @Query('major') major: string,
-    @Query() searchCoursesWithKeywordDto: SearchCoursesWithKeywordDto,
-  ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.searchMajorCourses(
-      major,
-      searchCoursesWithKeywordDto,
-    );
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('search-general')
-  async searchGeneralCourses(
-    @Query() searchCoursesWithKeywordDto: SearchCoursesWithKeywordDto,
-  ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.searchGeneralCourses(
-      searchCoursesWithKeywordDto,
-    );
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('search-academic-foundation')
-  async searchAcademicFoundationCourses(
-    @Query('college') college: string,
-    @Query() searchCoursesWithKeywordDto: SearchCoursesWithKeywordDto,
-  ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.searchAcademicFoundationCourses(
-      college,
-      searchCoursesWithKeywordDto,
-    );
-  }
-
-  // 학수번호 검색
-  @UseGuards(JwtAuthGuard)
-  @Get('search-course-code')
-  async searchCourseCode(
-    @Query() searchCourseCodeDto: SearchCourseCodeDto,
-  ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.searchCourseCode(searchCourseCodeDto);
-  }
-
-  // 전공 -- 과목명 검색
-  @UseGuards(JwtAuthGuard)
-  @Get('search-major-course-name')
-  async searchMajorCourseName(
-    @Query('major') major: string,
-    @Query() searchCourseNameDto: SearchCourseNameDto,
-  ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.searchMajorCourseName(
-      major,
-      searchCourseNameDto,
-    );
-  }
-
-  // 교양 - 과목명 검색
-  @UseGuards(JwtAuthGuard)
-  @Get('search-general-course-name')
-  async searchGeneralCourseName(
-    @Query() searchCourseNameDto: SearchCourseNameDto,
-  ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.searchGeneralCourseName(
-      searchCourseNameDto,
-    );
-  }
-
-  // 전공 - 교수님 성함 검색
-  @UseGuards(JwtAuthGuard)
-  @Get('search-major-professor-name')
-  async searchMajorProfessorName(
-    @Query('major') major: string,
-    @Query() searchProfessorNameDto: SearchProfessorNameDto,
-  ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.searchMajorProfessorName(
-      major,
-      searchProfessorNameDto,
-    );
-  }
-
-  // 교양 - 교수님 성함 검색
-  @UseGuards(JwtAuthGuard)
-  @Get('search-general-professor-name')
-  async searchGeneralProfessorName(
-    @Query() searchProfessorNameDto: SearchProfessorNameDto,
-  ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.searchGeneralProfessorName(
-      searchProfessorNameDto,
-    );
-  }
-
-  // 교양 리스트
-  @UseGuards(JwtAuthGuard)
-  @Get('general')
-  async getGeneralCourses(
-    @Query() getGeneralCourseDto: GetGeneralCourseDto,
-  ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.getGeneralCourses(getGeneralCourseDto);
-  }
-
-  // 전공 리스트 (학부별)
-  @UseGuards(JwtAuthGuard)
-  @Get('major')
-  async getMajorCourses(
-    @Query() getMajorCourseDto: GetMajorCourseDto,
-  ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.getMajorCourses(getMajorCourseDto);
-  }
-
-  // 학문의 기초 리스트
-  @UseGuards(JwtAuthGuard)
-  @Get('academic-foundation')
-  async getAcademicFoundationCourses(
-    @Query() getAcademicFoundationCourseDto: GetAcademicFoundationCourseDto,
-  ): Promise<PaginatedCoursesDto> {
-    return await this.courseService.getAcademicFoundationCourses(
-      getAcademicFoundationCourseDto,
-    );
-  }
-
-  @Get('/:courseId')
-  async getCourse(
-    @Param('courseId') courseId: number,
-  ): Promise<CommonCourseResponseDto> {
-    return await this.courseService.getCourse(courseId);
+    return await this.courseService.searchCourses(searchCourseNewDto);
   }
 }
--- src/decorators/docs/course.decorator.ts
+++ src/decorators/docs/course.decorator.ts
@@ -1,280 +1,60 @@
 import {
   ApiBearerAuth,
   ApiOperation,
-  ApiParam,
   ApiQuery,
   ApiResponse,
 } from '@nestjs/swagger';
 import { MethodNames } from 'src/common/types/method';
 import { CourseController } from 'src/course/course.controller';
-import { CommonCourseResponseDto } from 'src/course/dto/common-course-response.dto';
 import { PaginatedCoursesDto } from 'src/course/dto/paginated-courses.dto';
 import { ApiKukeyExceptionResponse } from '../api-kukey-exception-response';
 
 type CourseEndPoints = MethodNames<CourseController>;
 
 const CourseDocsMap: Record<CourseEndPoints, MethodDecorator[]> = {
-  searchAllCourses: [
+  searchCourses: [
     ApiBearerAuth('accessToken'),
     ApiOperation({
-      summary: 'keyword로 전체 강의 검색',
-      description: 'keyword를 입력하여 전체 강의에서 검색합니다.',
-    }),
-    ApiResponse({
-      status: 200,
-      description: 'keyword로 강의 검색 성공 시',
-      type: PaginatedCoursesDto,
-    }),
-  ],
-  searchMajorCourses: [
-    ApiBearerAuth('accessToken'),
-    ApiOperation({
-      summary: 'keyword로 전공 강의 검색',
-      description: 'keyword를 입력하여 전공 강의에서 검색합니다.',
+      summary: '강의 검색',
+      description: '하나의 엔드포인트로 모든 강의검색 로직을 통합했습니다.',
     }),
     ApiQuery({
-      name: 'major',
+      name: 'cursorId',
+      required: false,
+      type: 'number',
+    }),
+    ApiQuery({
+      name: 'year',
       required: true,
+      type: 'string',
+    }),
+    ApiQuery({
+      name: 'semester',
+      required: true,
+      type: 'string',
+    }),
+    ApiQuery({
+      name: 'category',
+      required: true,
+      type: 'enum',
+    }),
+    ApiQuery({
+      name: 'keyword',
+      required: false,
+      type: 'string',
+    }),
+    ApiQuery({
+      name: 'classification',
+      required: false,
       type: 'string',
     }),
     ApiResponse({
       status: 200,
-      description: 'keyword로 강의 검색 성공 시',
+      description: '강의 검색 성공 시',
       type: PaginatedCoursesDto,
     }),
     ApiKukeyExceptionResponse(['MAJOR_REQUIRED']),
-  ],
-  searchGeneralCourses: [
-    ApiBearerAuth('accessToken'),
-    ApiOperation({
-      summary: 'keyword로 교양 강의 검색',
-      description: 'keyword를 입력하여 교양 강의에서 검색합니다.',
-    }),
-    ApiResponse({
-      status: 200,
-      description: 'keyword로 강의 검색 성공 시',
-      type: PaginatedCoursesDto,
-    }),
-  ],
-  searchAcademicFoundationCourses: [
-    ApiBearerAuth('accessToken'),
-    ApiOperation({
-      summary: 'keyword로 학문의 기초 강의 검색',
-      description:
-        'keyword를 입력하여 단과대 별 학문의 기초 강의에서 검색합니다.',
-    }),
-    ApiQuery({
-      name: 'college',
-      required: true,
-      type: 'string',
-    }),
-    ApiResponse({
-      status: 200,
-      description: 'keyword로 강의 검색 성공 시',
-      type: PaginatedCoursesDto,
-    }),
     ApiKukeyExceptionResponse(['COLLEGE_REQUIRED']),
-  ],
-  searchCourseCode: [
-    ApiBearerAuth('accessToken'),
-    ApiOperation({
-      summary: '학수번호로 강의 검색',
-      description: '학수번호를 입력하여 강의를 검색합니다.',
-    }),
-    ApiQuery({
-      name: 'courseCode',
-      required: true,
-      type: 'string',
-    }),
-    ApiQuery({
-      name: 'cursorId',
-      required: false,
-      type: 'number',
-    }),
-    ApiResponse({
-      status: 200,
-      description: '학수번호로 강의 검색 성공 시',
-      type: PaginatedCoursesDto,
-    }),
-  ],
-  searchMajorCourseName: [
-    ApiBearerAuth('accessToken'),
-    ApiOperation({
-      summary: '전공 과목명 강의 검색',
-      description: '전공 과목명을 입력하여 강의를 검색합니다.',
-    }),
-    ApiQuery({
-      name: 'major',
-      required: true,
-      type: 'string',
-    }),
-    ApiQuery({
-      name: 'courseName',
-      required: true,
-      type: 'string',
-    }),
-    ApiQuery({
-      name: 'cursorId',
-      required: false,
-      type: 'number',
-    }),
-    ApiResponse({
-      status: 200,
-      description: '전공 과목명으로 강의 검색 성공 시',
-      type: PaginatedCoursesDto,
-    }),
-    ApiKukeyExceptionResponse(['MAJOR_REQUIRED']),
-  ],
-  searchGeneralCourseName: [
-    ApiBearerAuth('accessToken'),
-    ApiOperation({
-      summary: '교양 과목명 강의 검색',
-      description: '교양 과목명을 입력하여 강의를 검색합니다.',
-    }),
-    ApiQuery({
-      name: 'courseName',
-      required: true,
-      type: 'string',
-    }),
-    ApiQuery({
-      name: 'cursorId',
-      required: false,
-      type: 'number',
-    }),
-    ApiResponse({
-      status: 200,
-      description: '교양 과목명으로 강의 검색 성공 시',
-      type: PaginatedCoursesDto,
-    }),
-  ],
-  searchMajorProfessorName: [
-    ApiBearerAuth('accessToken'),
-    ApiOperation({
-      summary: '전공 과목 담당 교수님 성함으로 강의 검색',
-      description: '전공 과목 담당 교수님 성함을 입력하여 강의를 검색합니다.',
-    }),
-    ApiQuery({
-      name: 'major',
-      required: true,
-      type: 'string',
-    }),
-    ApiQuery({
-      name: 'professorName',
-      required: true,
-      type: 'string',
-    }),
-    ApiQuery({
-      name: 'cursorId',
-      required: false,
-      type: 'number',
-    }),
-    ApiResponse({
-      status: 200,
-      description: '전공 과목 담당 교수님 성함으로 강의 검색 성공 시',
-      type: PaginatedCoursesDto,
-    }),
-    ApiKukeyExceptionResponse(['MAJOR_REQUIRED']),
-  ],
-  searchGeneralProfessorName: [
-    ApiBearerAuth('accessToken'),
-    ApiOperation({
-      summary: '교양 담당 교수님 성함으로 강의 검색',
-      description: '교양 담당 교수님 성함을 입력하여 강의를 검색합니다.',
-    }),
-    ApiQuery({
-      name: 'professorName',
-      required: true,
-      type: 'string',
-    }),
-    ApiQuery({
-      name: 'cursorId',
-      required: false,
-      type: 'number',
-    }),
-    ApiResponse({
-      status: 200,
-      description: '교양 담당 교수님 성함으로 강의 검색 성공 시',
-      type: PaginatedCoursesDto,
-    }),
-  ],
-  getGeneralCourses: [
-    ApiBearerAuth('accessToken'),
-    ApiOperation({
-      summary: '교양 강의 조회',
-      description: '모든 교양 강의를 조회합니다.',
-    }),
-    ApiQuery({
-      name: 'cursorId',
-      required: false,
-      type: 'number',
-    }),
-    ApiResponse({
-      status: 200,
-      description: '교양 강의 조회 성공 시',
-      type: PaginatedCoursesDto,
-    }),
-  ],
-  getMajorCourses: [
-    ApiBearerAuth('accessToken'),
-    ApiOperation({
-      summary: '전공 강의 조회',
-      description: '해당 과의 모든 전공 강의를 조회합니다.',
-    }),
-    ApiQuery({
-      name: 'major',
-      required: true,
-      type: 'string',
-    }),
-    ApiQuery({
-      name: 'cursorId',
-      required: false,
-      type: 'number',
-    }),
-    ApiResponse({
-      status: 200,
-      description: '전공 강의 조회 성공 시',
-      type: PaginatedCoursesDto,
-    }),
-    ApiKukeyExceptionResponse(['MAJOR_REQUIRED']),
-  ],
-  getAcademicFoundationCourses: [
-    ApiBearerAuth('accessToken'),
-    ApiOperation({
-      summary: '학문의 기초 강의 조회',
-      description: '해당 단과대의 모든 학문의 기초 강의를 조회합니다.',
-    }),
-    ApiQuery({
-      name: 'college',
-      required: true,
-      type: 'string',
-    }),
-    ApiQuery({
-      name: 'cursorId',
-      required: false,
-      type: 'number',
-    }),
-    ApiResponse({
-      status: 200,
-      description: '학문의 기초 강의 조회 성공 시',
-      type: PaginatedCoursesDto,
-    }),
-    ApiKukeyExceptionResponse(['COLLEGE_REQUIRED']),
-  ],
-  getCourse: [
-    ApiOperation({
-      summary: '특정 강의 조회',
-      description: '특정 강의를 조회합니다.',
-    }),
-    ApiParam({
-      name: 'courseId',
-      description: '특정 강의 ID',
-    }),
-    ApiResponse({
-      status: 200,
-      description: '특정 강의 조회 성공 시',
-      type: CommonCourseResponseDto,
-    }),
-    ApiKukeyExceptionResponse(['COURSE_NOT_FOUND']),
   ],
 };
 
